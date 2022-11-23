1d084beed671476d9f72b90f98269d8547c04075
Jaakko <jiisala@gmail.com>
2022-11-23 12:59:34 +0200

    kahden tuotteen lisaaminen ksavattaa hintaa oikein

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -27,3 +27,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(empatia)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)       
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hinta(self):
+        maito = Tuote("Maito", 3)
+        empatia = Tuote("Empatia", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(empatia)
+        
+        self.assertEqual(self.kori.hinta(), 8)
+    
