9c790d1dc9d28d37c71cea02bbb2b83863023edf
Jaakko <jiisala@gmail.com>
2022-11-23 13:00:46 +0200

    kahden saman tuotteen lisaaminen kasvattaa ostoskoria oikein

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -28,6 +28,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(empatia)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)       
 
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)       
+
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hinta(self):
         maito = Tuote("Maito", 3)
         empatia = Tuote("Empatia", 5)
