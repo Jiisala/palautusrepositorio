c3a054112659bbf3ee42aaf20232ce0706cd174e
Jaakko <jiisala@gmail.com>
2022-11-23 13:33:00 +0200

    ostoskoriin voi lisata kaksi samaa tuotetta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -72,3 +72,12 @@ class TestOstoskori(unittest.TestCase):
         
         self.assertEqual(len(ostokset), 2)
     
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_yksi_tuotetta(self):
+        maito = Tuote("Maito", 3)
+        
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
+        
+        self.assertEqual(len(ostokset), 1)
+    
